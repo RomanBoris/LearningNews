f52ab8d7401c03e842e1ade513da87a58246f213
Roman <Krill.kron@yandex.ru>
2026-05-01 16:08:03 +0300

    add scaffold for fetailscreen and bottom back

--- app/src/main/java/com/pobezhkin/learningnews/presentation/detail/DetailNewsScreen.kt
+++ app/src/main/java/com/pobezhkin/learningnews/presentation/detail/DetailNewsScreen.kt
@@ -5,23 +5,69 @@ import android.net.Uri
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.viewinterop.AndroidView
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
-fun DetailNewsScreen(newsUrl: String) {
+fun DetailNewsScreen(
+    newsUrl: String,
+    onBack: () -> Unit
+) {
     val decoderUrl = Uri.decode(newsUrl)
-    AndroidView(
-        factory = { context ->
-            WebView(context).apply {
-                settings.javaScriptEnabled = true
-                webViewClient = WebViewClient()
-                loadUrl(decoderUrl)
-            }
+    Scaffold(
+        containerColor = Color.Transparent,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "НОВОСТИ",
+                        color = Color.Blue
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = onBack) {
+                        Icon(
+                            Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Назад",
+                            tint = Color.Blue
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Blue.copy(alpha = 0.7f)
+                )
+            )
+        }
+    ){ inner ->
 
-        },
-        modifier = Modifier.fillMaxSize()
-    )
-}
+        AndroidView(
+            modifier = Modifier.padding(inner).fillMaxSize(),
+            factory = { context ->
+                WebView(context).apply {
+                    settings.javaScriptEnabled = true
+                    webViewClient = WebViewClient()
+                    loadUrl(decoderUrl)
+                }
+
+            },
+
+        )
+    }
+
+    }
+
